da8f8ef11e4bf1dc5ecdd8d32cbe9f8a9efbc4e8
mtedderTechtonic <matthew.tedder@techtonic.com>
2019-06-10 22:50:40 -0600

    Create on click event
    When the button is clicked, an alert is fired to the page.

---
+++ jQuery-Challenge/index.js
@@ -0,0 +1,11 @@
+// Unsure of the difference between these, both work.
+
+$("button").on("click", function(){
+    alert("hello world")
+  })
+  
+  /*
+  $(document).on("click", "button", function(){
+    alert("hello world")
+  })
+  */
