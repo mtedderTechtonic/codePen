7693dcad96f616cfc2a94da93a08655afb73223d
Matt Tedder <matthew.tedder@techtonic.com>
2019-06-12 10:44:14 -0600

    Add element tags in comments

--- JS-Challenge/index.js
+++ JS-Challenge/index.js
@@ -5,13 +5,14 @@ var cities = [
     "Aurora"
   ]
   
+  // loop through the cities array
   for (var i = 0; i < cities.length; i++) {
-    // create the <li>
+    // create the <li> element
     var liElement = document.createElement("LI")
-    // append cities text to <li> element
+    // create cities text element
     var createCitiesText = document.createTextNode(cities[i])
-    
+    // append cities text to <li> element
     liElement.appendChild(createCitiesText)
-    // append li elements as a child of ul element
+    // append <li> elements as a child of <ul> element
     document.querySelector("ul.list-cities").appendChild(liElement)
   }
